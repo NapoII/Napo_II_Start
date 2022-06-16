c0e5dde65e35c61eafc8a448e9448a04543eafc0
NapoII <spacepioneers.4@hotmail.de>
2022-06-16 22:26:32 +0200

    v0.1
    Instal der Websiete

---
+++ config.js
@@ -0,0 +1,49 @@
+const CONFIG = {
+  hour12  : false
+}
+
+const ICONS = [
+  {
+    icon: "ri-terminal-box-fill",
+    link: "https://pr0gramm.com/top"
+  },
+  {
+    icon: "ri-youtube-fill",
+    link: "https://www.youtube.com/"
+  },
+  {
+    icon: "ri-twitch-fill",
+    link: "https://www.twitch.tv/"
+  },
+  {
+    icon: "ri-reddit-fill",
+    link: "https://www.reddit.com/"
+  },
+  {
+    icon: "ri-mail-send-fill",
+    link: "https://account.proton.me"
+  }
+]
+
+const LIST_ITEMS = [
+  {
+    name: "news.feed-reader",
+    link: "https://news.feed-reader.net/rubriken/ueberblick/top/"
+  },
+  {
+    name: "r/rbtv_cj",
+    link: "https://www.reddit.com/r/rbtv_cj/"
+  },
+  {
+    name: "TS3/rbtv/01",
+    link: "ts3server://rocketspeak.de?channel=%5Bcspacer0%5D%20%E2%95%90%20%5B%20%E2%98%85%20Lobby%20%E2%98%85%20%5D%20%E2%95%90"
+  },
+  {
+    name: "TS3/arpsel.ws/Talk II",
+    link: "ts3server://arpsel.ws?channel=%5Bcspacer8484%5D%D6%8D%20%C2%BB%20TALK%20%C2%AB%20%D6%8E%2F%D6%8D%20Talk%20II"
+  },
+]
+
+
+
+
